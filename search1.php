89372c74547cbeb08784d27d9d9ffd2e49917c2d
zeiadsalhin <zeyadali011@gmail.com>
2023-02-15 19:52:18 +0200

    v5

--- search1.php
+++ search1.php
@@ -23,51 +23,52 @@ if(isset($_POST['save']))
 ?>
 <html>
 <head>
-<title>ajax example</title>
+<title>Search Students</title>
 <link rel="stylesheet" href="bootstrap.css" crossorigin="anonymous">
-<!-- Optional theme -->
-<link rel="stylesheet" href="bootstrap-theme.css" crossorigin="anonymous">
+
 <style>
 .container{
     width:70%;
     height:30%;
-    padding:20px;
+    padding:10px;
+    background: fixed;
+    background-color: lightblue;
 }
 </style>
 </head>
  
 <body>
-    <div class="container">
-    <h3><u>PHP MySQL search database and display results</u></h3>
+    <div class="container ">
+    <h3><u>PHP MySQL search database</u></h3>
     <br/><br/>
     <form class="form-horizontal" action="#" method="post">
     <div class="row">
         <div class="form-group">
-            <label class="control-label col-sm-4" for="email"><b>Search Employee Information:</b>:</label>
+            <label class="control-label col-sm-4" for="email"><b>Search Students Information:</b>:</label>
             <div class="col-sm-4">
               <input type="text" class="form-control" name="search" placeholder="search here">
             </div>
-            <div class="col-sm-2">
-              <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+            
+            <div class="col-form-label">
+              <button type="submit" name="save" class="btn btn-primary">Submit</button>
             </div>
         </div>
-        <div class="form-group">
-            <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
-        </div>
+        
          
     </div>
     </form>
     <br/><br/>
-    <h3><u>Search Result</u></h3><br/>
+    <h3><u>Results</u></h3><br/>
     <div class="table-responsive">          
       <table class="table">
         <thead>
           <tr>
             <th>#</th>
-            <th>Employee Name</th>
+            <th>Student Name</th>
             <th>email</th>
-            <th>Age</th>
-            <th>Department</th>
+            <th>Level</th>
+            <th>Scores</th>
+            <th>DisplayScores</th>
           </tr>
         </thead>
         <tbody>
